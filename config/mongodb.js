0e4cdf681e89c33892739e65486e715fb053bb7e
akashi7 <christiannseko@gmail.com>
2024-08-06 20:39:22 +0200

    added socketio

---
+++ config/mongodb.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const connectDatabase = async () => {
+  try {
+    await prisma.$connect()
+    console.log('Database connected')
+  } catch (error) {
+    console.error('Failed to connect to database:', error)
+    process.exit(1)
+  }
+}
+
+const disconnectDatabase = async () => {
+  await prisma.$disconnect()
+  console.log('Database disconnected')
+}
+
+export { prisma, connectDatabase, disconnectDatabase }
